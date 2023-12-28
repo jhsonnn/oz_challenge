5d741505bfad85f0448fadc998d9e90a46220590
jhsonnn <jihyeongson13@gmail.com>
2023-12-28 16:35:42 +0900

    231228 js_challenge10 commit

--- javascript/challenge10/231228_특정문자탐색.js
+++ javascript/challenge10/231228_특정문자탐색.js
@@ -6,49 +6,46 @@
  * - 코드 실행 시 모든 테스트가 true인 경우 통과입니다!
  */
 
-/*  문제 출제
+/* 문제 출제
  *
- * - 우리는 숫자를 좋아하지만 홀수는 별로 좋아하지 않습니다.
- * - 그래서 우리는 주어진 숫자에서 홀수를 모두 빼고 남은 숫자들만 배열에 담아줍니다.
- *
- *❗️ 제한 조건 ❗️
- * - n은 자연수 입니다.
+ * - 당신은 탐험가로, 잊혀진 섬에 있는 보물을 찾아 모험을 떠납니다.
+ * - 보물은 숨겨진 곳에 있으며, 특정 문자를 가진 보물을 찾을 때마다 보물의 흔적이 나타납니다.
+ * - 보물은 총 몇개인지 문자열에서 특정 문자의 개수를 세어 확인해보세요!
  *
  *
- * # 예시
- *- n이 5인 경우 결과 값 [2,4]
+ *
+ *
  */
 
- function question(n) {
+function question(a, n) {
     // 여기에서 코드 작성해주세요!
-    let result = [];
- 
-    for(let i=0; i<=n; i++){
-        if(i%2 === 0){
-            result.push(i)
+    let array = a;
+    let result = 0;
+
+    for(let i = 0; i < array.length; i++){
+        if(array[i]===n){
+            result++;
         }
     }
-
     return result;
 }
 
 // 여기는 결과값 함수이므로 신경쓰지 않으셔도 됩니다!
 Test(
     question,
-    [[10], [3], [8], [6], [9]],
     [
-        [0, 2, 4, 6, 8, 10],
-        [0, 2],
-        [0, 2, 4, 6, 8],
-        [0, 2, 4, 6],
-        [0, 2, 4, 6, 8],
-    ]
+        [['h', 'a', 'p', 'p', 'y'], 'p'],
+        [['h', 'e', 'l', 'l', 'o'], 'o'],
+        [['c', 'h', 'r', 'i', 's', 't', 'm', 'a', 's'], 'a'],
+        [['네', '잎', '클', '로', '버', '로', '버', '오', '버'], '버'],
+    ],
+    [2, 1, 1, 3]
 );
 
 function Test(question, conditions, results) {
     for (let index in results) {
         const result = Array.isArray(results[index])
-            ? JSON.stringify(question(...conditions[index])) === JSON.stringify(results[index])
+            ? JSON.stringify([question(...conditions[index])]) === JSON.stringify(results[index])
             : question(...conditions[index]) === results[index];
         console.log(`테스트 ${+index + 1}`, result);
         if (!result) {
